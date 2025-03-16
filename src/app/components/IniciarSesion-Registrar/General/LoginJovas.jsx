3721e3a450c48f4ddfb21f0810fef8238340338e
Judith Martínez <aguimtz.2003@gmail.com>
2025-03-16 00:12:14 -0600

    RI-27 Login-backend-100%
    Se agrego un login provisional para realizar pruebas.

--- src/app/components/IniciarSesion-Registrar/General/LoginJovas.jsx
+++ src/app/components/IniciarSesion-Registrar/General/LoginJovas.jsx
@@ -29,7 +29,7 @@ const LoginJovas = () => {
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-2xl">
         <h2 className="text-2xl font-bold text-center text-gray-800">
-          Iniciar Sesión Judito & Chuchas
+          Iniciar Sesión Jovas
         </h2>
         {error && <p className="text-red-500 text-center">{error}</p>}
         <form className="space-y-4" onSubmit={handleSubmit}>
