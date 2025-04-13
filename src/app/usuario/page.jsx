3d82501172349a43c5678806185eb9706a4f57b6
JovanyEquihua <equicerjoal@gmail.com>
2025-04-13 15:30:15 -0600

    Actualizacion

--- src/app/usuario/page.jsx
+++ src/app/usuario/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Filtrar from '../recetas/Filtrar'
 
 const page = () => {
   return (
     <div className="p-4">
     <h2 className="text-xl font-bold">Bienvenido al panel de usuario</h2>
     <p>Aquí puedes gestionar tu cuenta y ver tus datos.</p>
+    <Filtrar/>
   </div>
   )
 }
