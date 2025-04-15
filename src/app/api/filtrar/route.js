ff3ad303a6bb3348dee130189f6a219aea2d1349
Judith Martínez <aguimtz.2003@gmail.com>
2025-04-15 00:08:52 -0600

    RI-50 <backend-filtrar-receta-100%>
    Filtrado de recetar por
    -TipoComida
    -Dificultad
    -TiempoPreparacion
    -Calificaciones
    -TipoSabor
    -Porciones
    **ejemplos de como manejarlo en frontend en \Banner\index.jsx

--- src/app/api/filtrar/route.js
+++ src/app/api/filtrar/route.js
@@ -1,33 +1,73 @@
-//API para filtrar datos de la base de datos " receta "
+//API para filtrar recetas
 
 import { db } from "../../../libs/db";
 
+/* Este endpoint se encarga de filtrar 
+  recetas según los criterios proporcionados
+  en el cuerpo de la solicitud */
+
+
 export async function POST(req) {
-    try {
-      const { tipoComida  } = await req.json();
-  
-      // mostrar datos de la bd
-      const resultados = await db.receta.findMany({
-        where: {
-            tipoComida: {
-              nombre: tipoComida,
-            },
-          },
-          include: {
-            tipoComida: true,
-          },
-        });
-  
-  
-      return new Response(JSON.stringify(resultados), {
-        status: 200,
-        headers: { "Content-Type": "application/json" }
-      });
-    } catch (error) {
-      console.error("Error en el filtrado:", error);
-      return new Response(JSON.stringify({ error: "Error en el filtrado" }), {
-        status: 500
-      });
+  try {
+    // Obtener los filtros del cuerpo de la solicitud
+    const filtrarRecetas = await req.json();
+
+   //Verificar los filtros
+    const filtros = {};
+
+      // Si se envía el filtro de tipo de comida, lo agrega al objeto de filtros
+    if (filtrarRecetas.tipoComida) {
+      filtros.tipoComida = {
+        nombre: filtrarRecetas.tipoComida,
+      };
     }
+  // Si se envía el filtro de dificultad, lo agrega al objeto de filtros
+    if (filtrarRecetas.dificultad) {
+      filtros.dificultad = filtrarRecetas.dificultad;
+      
+    }
+// Si se envía el filtro de tiempo de preparación, lo agrega al objeto de filtros
+    if (filtrarRecetas.tiempoPreparacion) {
+      filtros.tiempoPreparacion = 
+        parseInt(filtrarRecetas.tiempoPreparacion);
+      
+    }
+// Si se envía el filtro de calificaciones, lo agrega al objeto de filtros
+    if (filtrarRecetas.calificaciones) {
+      filtros.calificaciones = {
+        puntuacion: filtrarRecetas.calificaciones,
+      };
+    }
+// Si se envía el filtro de tipo de sabor, lo agrega al objeto de filtros
+    if (filtrarRecetas.tipoSabor) {
+      filtros.tipoSabor = {
+        nombreSabor: filtrarRecetas.tipoSabor,
+      };
+    }
+ // Si se envía el filtro de porciones, lo agrega al objeto de filtros
+    if (filtrarRecetas.porciones) {
+      filtros.porciones = parseInt(filtrarRecetas.porciones);
+    }
+
+  //Realiza la consulta a la base de datos con los filtros construidos
+
+    const resultados = await db.receta.findMany({
+      where: filtros, // Aplica los filtros
+      include: {
+        tipoComida: true, // Incluye información relacionada con el tipo de comida
+        calificaciones: true, // Incluye información relacionada con las calificaciones
+        tipoSabor: true, // Incluye información relacionada con el tipo de sabor
+      },
+    });
+
+    return new Response(JSON.stringify(resultados), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error en el filtrado:", error);
+    return new Response(JSON.stringify({ error: "Error en el filtrado" }), {
+      status: 500,
+    });
   }
-  
+}
--- src/app/components/Banner/index.jsx
+++ src/app/components/Banner/index.jsx
@@ -4,35 +4,30 @@ import { Fade } from "react-awesome-reveal";
 import Link from "next/link";
 import React, { useState } from "react";
 
-
-
-
 const index = () => {
 
-//Ejemplo de como usar el filtrador (api/filtrar)
-const [recetas, setRecetas] = useState();
-
-const handleFiltrar = async (tipoComida) => {
-  try {
-    const res = await fetch("/api/filtrar", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-     
-      body: JSON.stringify({ tipoComida }),
-    });
-
-    const data = await res.json();
-    console.log(data); // Verifica la respuesta de la API
-    setRecetas(data); // Actualizas el estado con las recetas
-  } catch (error) {
-    console.error("Error al filtrar recetas:", error);
-  }
-};
-
-
-
+  /* Ejemplo de como usar el filtrador (api/filtrar)
+  const [recetas, setRecetas] = useState();
+  const handleFiltrar = async (filtros) => {
+    try {
+      const res = await fetch("/api/filtrar", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(filtros), // Envía directamente el objeto
+       
+      }
+    );
+  
+      const data = await res.json();
+     console.log("Recetas filtradas:", data); // Verifica los datos recibidos
+      setRecetas(data);
+    } catch (error) {
+      console.error("Error al filtrar recetas:", error);
+    }
+  };
+*/
   return (
     <div id="home-section" className="bg-[#F8F7F7]">
       <div className="mx-auto max-w-7xl  pt-20 sm:pb-24 px-6 ">
@@ -72,13 +67,20 @@ const handleFiltrar = async (tipoComida) => {
                 <button className="text-xl w-full md:w-auto font-medium rounded-full text-black py-5 px-6 bg-pink lg:px-14 mr-6">
                   <Link href="#cook-section">Lets cook</Link>
                 </button>
-                {/* Ejemplo de como usar el filtrador (api/filtrar) */}
                 <button
-                  onClick={() => handleFiltrar("Mexicana")}
+                  onClick={() => handleFiltrar({ tipoComida: "Mexicana" })}
+                  className="flex border w-full md:w-auto mt-5 md:mt-0 border-gray-400 justify-center rounded-full text-xl font-medium items-center py-5 px-10 text-pink hover:text-white hover:bg-pink"
+                >
+                 Explore
+                </button>
+                {/* Ejemplo de como usar el filtrador (api/filtrar) */}
+                {/* <button
+                  onClick={() => handleFiltrar({ tipoComida: "Mexicana" })}
                   className="flex border w-full md:w-auto mt-5 md:mt-0 border-orange-500 justify-center rounded-full text-xl font-medium items-center py-5 px-10 text-pink hover:text-white hover:bg-pink"
                 >
                   Mexicana
                 </button>
+              */}
               </div>
             </Fade>
           </div>
