89789da0fc55f0a2eb457d495232bf697891ff90
JovanyEquihua <equicerjoal@gmail.com>
2025-05-18 18:26:10 -0600

    Modificaciones

--- src/app/components/Animaciones/CookingLoader.jsx
+++ src/app/components/Animaciones/CookingLoader.jsx
@@ -1,21 +1,18 @@
 "use client";
 
 import React from "react";
-import Lottie from "lottie-react";
+import Lottie from "react-lottie-player";
+import animationData from "../../../../public/animacion.json"; // Cambia la ruta según tu archivo
 
 function CookingLoader() {
-  const defaultOptions = {
-    loop: true, // Repetir la animación
-    autoplay: true, // Iniciar automáticamente
-    animationData: require("../../../../public/animacion.json"), // Cambia la ruta
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice", // Ajuste de responsividad
-    },
-  };
-
   return (
     <div style={{ width: "200px", margin: "0 auto" }}>
-      <Lottie options={defaultOptions} />
+      <Lottie
+        loop
+        animationData={animationData}
+        play
+        style={{ height: "200px", width: "200px" }}
+      />
       <p style={{ textAlign: "center" }}>¡Cocinando algo delicioso! 🍳</p>
     </div>
   );
