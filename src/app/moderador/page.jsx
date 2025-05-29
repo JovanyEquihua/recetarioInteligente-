0c3b64e46a7ea5e1749ed79bfdafd1d7ae177761
JovanyEquihua <equicerjoal@gmail.com>
2025-05-28 21:12:12 -0600

    Layout y migración

---
+++ src/app/moderador/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="p-4">
+    <h2 className="text-xl font-bold">Bienvenido al panel de moderador</h2>
+    <p>Aquí puedes gestionar las cuentas,rectas y mas.</p>
+  </div>
+  )
+}
+
+export default page
